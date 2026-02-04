ceab6bd9e98827f8b7e9617e111faad128ac3670
yulisabelle <yuliannamen28@gmail.com>
2026-02-04 18:39:14 +0000

    c

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -5,14 +5,15 @@
 #include <iostream>
 #include <cstdint>
 
-enum cow_purpose {dairy, meat, hide, pet};
+enum class cow_purpose {dairy, meat, hide, pet};
+enum class grocery_section {canned, forzen , meat, laundry, dairy, bakery};
 
 int main(){
-    int a;
+    cow_purpose a;
 
-    a = meat;
+    a = cow_purpose::meat;
 
-    std::cout << "a = " << a << std::endl;
+    std::cout << "a = " << (int) a << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
